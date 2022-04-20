4afbace58a68d3d5ca6c8843a04f6cb131490143
ashcolor <ashcolor27@gmail.com>
2022-04-20 23:44:50 +0900

    del: 不要コードの削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from "nuxt3";
 
 export default defineNuxtConfig({
-  css: ["@/assets/css/tailwind.css", "~/assets/fontawesome/all.min.css"],
+  css: ["@/assets/css/tailwind.css"],
   build: {
     postcss: {
       postcssOptions: {
