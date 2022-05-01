a5a0a0ad46d792d4093677ff95710452a2b48f3b
ashcolor <ashcolor27@gmail.com>
2022-05-01 16:07:40 +0900

    chg: hooks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,15 @@ export default defineNuxtConfig({
   router: {
     base: "/google-maps-api-demo/dist/",
   },
+  hooks: {
+    "pages:extend"(pages) {
+      pages.push({
+        name: "gh-pages",
+        path: "/",
+        file: resolve(__dirname, "pages/index.vue"),
+      });
+    },
+  },
   css: ["@/assets/css/tailwind.css"],
   postcss: {
     plugins: {
